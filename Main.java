3f66b926011f04b3320c4b8f76ce1047580e53dd
Roey Erel <88533848+RoeyErel@users.noreply.github.com>
2022-04-02 11:59:57 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,7 +16,10 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Main {
-
+	
+	/**
+	 * 
+	 */
 	private JFrame frame;
 	private String _nextPlayer = "F";
 	private int _movesMade = 0;
@@ -51,35 +54,41 @@ public class Main {
 	}
 	
 	/**
-	 *  rows 012 345 678
-	 *  columns 036 147 258
-	 *  diagonals 048 246 
+	 * The method checking if one of the users win.
+	 * Possible win can get by the following sequences.
+	 * Board numbered as
+	 * 0 1 2
+	 * 3 4 5
+	 * 6 7 8
+	 * rows sequence: 012 345 678
+	 * columns sequence: 036 147 258
+	 * diagonals sequence: 048 246 
 	 */
 	private void winGame() {
 	    /**
-	     * for case that only X win
+	     * Checking X player possible win.
 	     * 
 	     */
 	    /**
-	     * rows 012 345 678
+	     * Check possible row sequence at rows 012 345 678.
 	     */
 	    if (gameBoard[0] == 1 && gameBoard[1] == 1 && gameBoard[2] == 1 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 1 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
 	        _player1Score++;
 	        xScore.setText(String.valueOf(_player1Score));
-	        
-	        
+	           
 	    }else if (gameBoard[3] == 1 && gameBoard[4] == 1 && gameBoard[5] == 1 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 1 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
 	        _player1Score++;
 	        xScore.setText(String.valueOf(_player1Score));
+	        
 	    }else if (gameBoard[6] == 1 && gameBoard[7] == 1 && gameBoard[8] == 1 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 1 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
 	        _player1Score++;
 	        xScore.setText(String.valueOf(_player1Score));
 	    }
 	    /**
-	     * columns 036 147 258
+	     * Check possible columns sequence at columns 036 147 258.
 	     */
 	    else if (gameBoard[0] == 1 && gameBoard[3] == 1 && gameBoard[6] == 1 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 1 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
@@ -97,7 +106,7 @@ public class Main {
 	        xScore.setText(String.valueOf(_player1Score));
 	    }
 	    /**
-	     * diagonals 048 246
+	     * Check possible diagonals sequence at diagonals 048 246.
 	     */
 	    else if (gameBoard[0] == 1 && gameBoard[4] == 1 && gameBoard[8] == 1 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 1 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
@@ -110,10 +119,10 @@ public class Main {
 	        xScore.setText(String.valueOf(_player1Score));
 	    }
 	    /**
-	     * for case that only O win
+	     * Checking O player possible win.
 	     */
 	    /**
-	     * rows 012 345 678
+	     * Check possible row sequence at rows 012 345 678.
 	     */
 	    else if (gameBoard[0] == 2 && gameBoard[1] == 2 && gameBoard[2] == 2 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 2 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
@@ -131,7 +140,7 @@ public class Main {
 	        oScore.setText(String.valueOf(_player2Score));
 	    }
 	    /**
-	     * columns 036 147 258
+	     * Check possible columns sequence at columns 036 147 258.
 	     */
 	    else if (gameBoard[0] == 2 && gameBoard[3] == 2 && gameBoard[6] == 2 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 2 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
@@ -149,7 +158,7 @@ public class Main {
 	        oScore.setText(String.valueOf(_player2Score));
 	    
 	    /**
-	     * diagonals 048 246
+	     * Check possible diagonals sequence at diagonals 048 246.
 	     */
 	    }else if (gameBoard[0] == 2 && gameBoard[4] == 2 && gameBoard[8] == 2 ) {
 	        JOptionPane.showMessageDialog(frame, "Player 2 Win", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
@@ -164,7 +173,7 @@ public class Main {
 	    }else if (_movesMade ==9 ){
 	    	JOptionPane.showMessageDialog(frame, "Tied", ".", JOptionPane.INFORMATION_MESSAGE);
 	    }
-	}
+	}// end of winGame method
 	
 	/**
 	 * Initialize the contents of the frame.
@@ -179,16 +188,15 @@ public class Main {
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(new Color(77, 77, 77)));
 		panel.add(panel_1);
 		panel_1.setLayout(new BorderLayout(0, 0));
 		
-
-		
 		/**
-		 * Button 1
+		 * The first button first row at the first column
+		 * @actionPerformed place a X/O on the board.
 		 */
 		JButton oneOne = new JButton();
 		oneOne.setFont(new Font("Tahoma", Font.PLAIN, 75));
@@ -226,9 +234,10 @@ public class Main {
 		panel_2.setBorder(new LineBorder(new Color(77, 77, 77)));
 		frame.getContentPane().add(panel_2);
 		panel_2.setLayout(new BorderLayout(0, 0));
-		
+
 		/**
-		 * Button 2
+		 * The second button first row at the second column
+		 * @actionPerformed place a X/O on the board.
 		 */
 		JButton oneTwo = new JButton();
 		oneTwo.setFont(new Font("Tahoma", Font.PLAIN, 75));
@@ -260,15 +269,16 @@ public class Main {
 				winGame();
 			}
 		});
-		
 		panel_2.add(oneTwo, BorderLayout.CENTER);
-		
+
 		JPanel panel_3 = new JPanel();
 		panel_3.setBorder(new LineBorder(new Color(77, 77, 77)));
 		frame.getContentPane().add(panel_3);
 		panel_3.setLayout(new BorderLayout(0, 0));
+		
 		/**
-		 * 
+		 * The third button first row at the third column
+		 * @actionPerformed place a X/O on the board.
 		 */
 		JButton oneThree = new JButton("");
 		oneThree.setBackground(new Color(60, 179, 113));
@@ -306,6 +316,10 @@ public class Main {
 		frame.getContentPane().add(panel_4);
 		panel_4.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The fourth button second row at the first column
+		 * @actionPerformed place a X/O on the board.
+		 */
 		JButton twoOne = new JButton("");
 		twoOne.setBackground(new Color(60, 179, 113));
 		twoOne.addActionListener(new ActionListener() {
@@ -342,6 +356,10 @@ public class Main {
 		frame.getContentPane().add(panel_5);
 		panel_5.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The fifth button second row at the second column
+		 * @actionPerformed place a X/O on the board.
+		 */
 		JButton twoTwo = new JButton("");
 		twoTwo.setBackground(new Color(60, 179, 113));
 		twoTwo.addActionListener(new ActionListener() {
@@ -378,6 +396,10 @@ public class Main {
 		frame.getContentPane().add(panel_6);
 		panel_6.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The sixth button second row at the third column
+		 * @actionPerformed place a X/O on the board.
+		 */
 		JButton twoThree = new JButton("");
 		twoThree.setBackground(new Color(60, 179, 113));
 		twoThree.addActionListener(new ActionListener() {
@@ -414,6 +436,10 @@ public class Main {
 		frame.getContentPane().add(panel_7);
 		panel_7.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The seventh button third row at the first column
+		 * @actionPerformed place a X/O on the board.
+		 */
 		JButton threeOne = new JButton("");
 		threeOne.setBackground(new Color(60, 179, 113));
 		threeOne.addActionListener(new ActionListener() {
@@ -450,6 +476,10 @@ public class Main {
 		frame.getContentPane().add(panel_8);
 		panel_8.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The eighth  button third row at the second column
+		 * @actionPerformed place a X/O on the board.
+		 */
 		JButton ThreeTwo = new JButton("");
 		ThreeTwo.setBackground(new Color(60, 179, 113));
 		ThreeTwo.addActionListener(new ActionListener() {
@@ -486,6 +516,10 @@ public class Main {
 		frame.getContentPane().add(panel_9);
 		panel_9.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The ninth  button third row at the third column
+		 * @actionPerformed place a X/O on the board.
+		 */
 		JButton threeThree = new JButton("");
 		threeThree.setBackground(new Color(60, 179, 113));
 		threeThree.addActionListener(new ActionListener() {
@@ -521,6 +555,9 @@ public class Main {
 		frame.getContentPane().add(panel_10);
 		panel_10.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * First player, the X player
+		 */
 		player1 = new JTextField("PLAYER 1");
 		player1.setFont(new Font("Calibri", Font.BOLD, 30));
 		player1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -532,6 +569,9 @@ public class Main {
 		frame.getContentPane().add(panel_11);
 		panel_11.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * Second player, the O player
+		 */
 		player2 = new JTextField("PLAYER 2");
 		player2.setFont(new Font("Calibri", Font.BOLD, 30));
 		player2.setHorizontalAlignment(SwingConstants.CENTER);
@@ -543,6 +583,10 @@ public class Main {
 		frame.getContentPane().add(exitPanel);
 		exitPanel.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * The exit button, after click show dialog with Yes/No option
+		 * @mouseClicked Yes/No option, yes for exit the program.
+		 */
 		JButton exitButton = new JButton("EXIT");
 		exitButton.setBackground(Color.WHITE);
 		exitButton.setForeground(new Color(255, 0, 51));
@@ -563,6 +607,9 @@ public class Main {
 		frame.getContentPane().add(panel_14);
 		panel_14.setLayout(new BorderLayout(0, 0));
 		
+		/**
+		 * Represent the score of the X player
+		 */
 		xScore = new JTextField();
 		xScore.setHorizontalAlignment(SwingConstants.CENTER);
 		xScore.setForeground(Color.BLACK);
@@ -574,7 +621,9 @@ public class Main {
 		panel_13.setBorder(new LineBorder(Color.WHITE, 2));
 		frame.getContentPane().add(panel_13);
 		panel_13.setLayout(new BorderLayout(0, 0));
-		
+		/**
+		 * Represent the score of the X player
+		 */
 		oScore = new JTextField();
 		oScore.setHorizontalAlignment(SwingConstants.CENTER);
 		oScore.setForeground(Color.RED);
@@ -586,7 +635,10 @@ public class Main {
 		restPanel.setBorder(new LineBorder(Color.WHITE, 2));
 		frame.getContentPane().add(restPanel);
 		restPanel.setLayout(new BorderLayout(0, 0));
-		
+		/**
+		 * Rest the game, board and the keep score.
+		 * @mouseClicked restart the game.
+		 */
 		JButton restButton = new JButton("REST");
 		restButton.setBackground(Color.WHITE);
 		restButton.setForeground(new Color(51, 102, 255));
@@ -616,6 +668,7 @@ public class Main {
 			}
 		});
 		restButton.setFont(new Font("Calibri", Font.BOLD, 30));
-		restPanel.add(restButton, BorderLayout.CENTER);
+		restPanel.add(restButton, BorderLayout.CENTER);	
+		
 	}
 }
